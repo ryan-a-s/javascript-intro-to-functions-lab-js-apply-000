69480874b3fad6610353e78a585249de12bf8b4a
Ryan Seit <ryan@ryanseit.com>
2020-01-16 18:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return = "YES INDEED!";
   }
-  if (string.)
+  if (string === "I love you, Grandma.") {
+    return = "I love you, too.";
+  }
 }
-  
-
-function sayHiToGrandma()
