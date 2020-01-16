f9ba058c14bdbb8ee2602ef621ea76c083c15ea3
Ryan Seit <ryan@ryanseit.com>
2020-01-16 18:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return 
+    return = "I can\'t hear you!";
   }
+  if (string.toUpperCase() === string) {
+    return = "YES INDEED!";
+  }
+  if (string.)
 }
   
 
